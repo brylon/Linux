6af165a5f7d7a9cf54390fd92b5ed4b59363fd9d
brylon <57745819+brylon@users.noreply.github.com>
2021-06-27 16:21:23 +0800

    Add files via upload

---
+++ FTP_system/test.h
@@ -0,0 +1,12 @@
+#define MAXLINE 4096
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <errno.h>
